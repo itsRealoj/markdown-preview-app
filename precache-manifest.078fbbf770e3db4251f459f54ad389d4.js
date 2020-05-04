7162217642499ec4c7496bc4eeebb4be50f7a933
itsrealoj <stephenojwang1040@gmail.com>
2020-05-04 22:05:52 +0300

    Updates

---
+++ precache-manifest.078fbbf770e3db4251f459f54ad389d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33ee9a2e6ca0df557c817c8858b133f",
+    "url": "/markdown-preview-app/index.html"
+  },
+  {
+    "revision": "9e10156d860a916b89a5",
+    "url": "/markdown-preview-app/static/css/main.4e213f89.chunk.css"
+  },
+  {
+    "revision": "112cffc2b13b704b8edf",
+    "url": "/markdown-preview-app/static/js/2.ed22aa3d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/markdown-preview-app/static/js/2.ed22aa3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e10156d860a916b89a5",
+    "url": "/markdown-preview-app/static/js/main.d8c9ff76.chunk.js"
+  },
+  {
+    "revision": "807e9663d696c7ee2190",
+    "url": "/markdown-preview-app/static/js/runtime-main.82f33870.js"
+  }
+]);
